674184c9224af2128b06d1c7625962b645a1c423
joaoduarte01 <joao_duarte3@estudante.sesisenai.org.br>
2025-04-17 09:06:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-function validateForm() {
-    let x = document.forms["myForm"]["fname"].value;
-    if (x == "") {
-      alert("Name must be filled out");
-      return false;
-    }
+function validarFormulario() {
+  const cpf = document.getElementById('cpf').value;
+  const telefone = document.getElementById('telefone').value;
+  const email = document.getElementById('email').value;
+
+  if (!cpfRegex.test(cpf)) {
+    alert("CPF inválido. Use o formato 000.000.000-00.");
+    return false;
   }
+
+  if (!telefoneRegex.test(telefone)) {
+    alert("Telefone inválido. Use o formato (00) 00000-0000.");
+    return false;
+  }
+
+  if (!emailRegex.test(email)) {
+    alert("E-mail inválido.");
+    return false;
+  }
+
+  alert("Formulário enviado com sucesso!");
+  return true;
+}
